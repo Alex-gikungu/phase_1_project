41dcb2f66c0c3ceeec2a8625d930d61b4a3ad9a5
Alex-gikungu <alexigikungu.012@gmail.com>
2023-07-07 16:18:06 +0300

    added eventlistener to the send button in the contact section to perform the following prompt fill all sections if not filled if all sections are filled promt sent successfull refresh the inputs when all the inputs are filled  correctly and the button is clicked

--- script.js
+++ script.js
@@ -1,59 +1,24 @@
-// let sendButton = document.getElementById('btns');
-// sendButton.addEventListener('click', () => {
-//   let nameInput = document.getElementById('name');
-//   let emailInput = document.getElementById('email');
-//   let messageInput = document.getElementById('message');
+let sendButton = document.getElementById('btns');
+sendButton.addEventListener('click', () => {
+  let nameInput = document.getElementById('name');
+  let emailInput = document.getElementById('email');
+  let messageInput = document.getElementById('message');
 
-//   // Check if inputs are filled correctly
-//   if (
-//     nameInput.value.trim() !== '' &&
-//     emailInput.value.trim() !== '' &&
-//     messageInput.value.trim() !== ''
-//   ) {
-//     // Display success message
-//     let confirmation = confirm('Submit successful. Do you want to delete the house?');
+  // Check if inputs are filled correctly
+  if (
+    nameInput.value.trim() !== '' &&
+    emailInput.value.trim() !== '' &&
+    messageInput.value.trim() !== ''
+  ) {
+    // Display success message
+    alert('Submit successful');
 
-//     if (confirmation) {
-//       // Delete the house from the server
-//       deleteHouseFromServer(id);
-//     }
-
-//     // Reset the input values
-//     nameInput.value = '';
-//     emailInput.value = '';
-//     messageInput.value = '';
-//   } else {
-//     // Prompt alert for incorrect details
-//     alert('Fill all the inputs correctly');
-//   }
-// });
-
-// function deleteHouseFromServer(id) {
-//   fetch(`http://localhost:3000/houses/${id}`, {
-//     method: "DELETE",
-//   })
-//     .then(response => response.json())
-//     .then(data => {
-//       // Handle the server response if needed
-//       console.log("House deleted:", data);
-
-//       // Remove the house element from the DOM
-//       newHouse.remove();
-
-//       // Redirect to contact.html
-//       window.location.href = "contact.html";
-//     })
-//     .catch(error => console.log(error));
-// }
-
-
-
-
-
-
-
-
-
-
-
-
+    // Reset the input values
+    nameInput.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
+  } else {
+    // Prompt alert for incorrect details
+    alert('Fill all the inputs correctly');
+  }
+});
